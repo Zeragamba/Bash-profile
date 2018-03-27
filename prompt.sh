fce8a89014dd27de2b0e39dd17bcc5f3aa1048b3
Stephen Wilson <swilson2@opentext.com>
2018-03-27 13:37:08 -0400

    rewrite prompt

--- prompt.sh
+++ prompt.sh
@@ -1,1 +1,21 @@
-PS1='\[\e[0;37m\]┬─╢\[\e[4;37m\] \h \[\e[0;37m\]│\[\e[4;32m\] \w \[\e[0;37m\]$(__git_ps1 "│\[\e[4;31m\] %s \[\e[0;37m\]")│\[\e[4;36m\] \@ \[\e[0;37m\]╟──\n╰─\$ '
+PROMPT_COMMAND=__prompt_command
+__prompt_command() {
+	local EXIT="$?"
+	PS1=""
+
+	local Cnorm='\[\e[0;37m\]'
+	local Chost='\[\e[4;37m\]'
+	local Cpath='\[\e[4;32m\]'
+	local Cgit='\[\e[4;31m\]'
+	local Ctime='\[\e[4;36m\]'
+	local Cexit='\[\e[4;35m\]'
+	
+	PS1+="${Cnorm}┬─╢"
+
+	PS1+="${Chost} \h ${Cnorm}|"
+	PS1+="${Cpath} \w ${Cnorm}|"
+	PS1+="$(__git_ps1 "${Cgit} %s ${Cnorm}|")"
+	PS1+="${Cexit} ${EXIT} ${Cnorm}"
+	
+	PS1+="${Cnorm}╟──\n╰─\$ "
+}
