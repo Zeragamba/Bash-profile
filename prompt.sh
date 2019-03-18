a35826582c4dbaaf8298bb9d6fc6c46c9092b071
Stephen A. Wilson <Stephen-356@hotmail.com>
2019-03-18 18:32:40 -0400

    add toggle-able language info

--- prompt.sh
+++ prompt.sh
@@ -1,4 +1,13 @@
-PROMPT_COMMAND=__prompt_command
+prompt-langs() {
+	if [ ! -z "$1" ] && [ $1 = off ]; then
+		echo 'false' > ~/.bash/.prompt-langs
+	else
+		echo 'true' > ~/.bash/.prompt-langs
+	fi
+
+	PROMPT_SHOW_LANGS=$(cat ~/.bash/.prompt-langs)
+}
+
 __prompt_command() {
 	local EXIT="$?"
 	
@@ -10,23 +19,39 @@ __prompt_command() {
 	local Clang='\[\e[4;91m\]'
 	local Cexit='\[\e[4;35m\]'
 	
-	PS1="${Cnorm}┬─╢"
+	PS1="${Cnorm}┬─┤"
 
+	# Hostname
 	PS1+="${Chost} \h ${Cnorm}|"
+
+	# Working directory
 	PS1+="${Cpath} \w ${Cnorm}|"
+
+	# Git Info
 	PS1+="$(__git_ps1 "${Cgit} %s ${Cnorm}|")"
 	PS1+="${Ctime} \@ ${Cnorm}|"
 
-	# languages
-	if [ -x "$(command -v ruby)" ]; then
-		PS1+="${Clang} Ruby: $(ruby -e "puts RUBY_VERSION") ${Cnorm}|"
+	# Languages
+	if [ "$PROMPT_SHOW_LANGS" = true ]; then
+		# languages
+		if [ -x "$(command -v ruby)" ]; then
+			PS1+="${Clang} Ruby:$(ruby -e "puts RUBY_VERSION") ${Cnorm}│"
+		fi
+
+		if [ -x "$(command -v node)" ]; then
+			PS1+="${Clang} Node:$(node -e "console.log(process.version)") ${Cnorm}│"
+		fi
 	fi
 
-	if [ -x "$(command -v node)" ]; then
-		PS1+="${Clang} Node: $(node -e "console.log(process.version)") ${Cnorm}|"
-	fi
-	
-	PS1+="${Cexit} ${EXIT} ${Cnorm}"
-	
-	PS1+="${Cnorm}╟──\n╰─\$ "
+	# Time
+	PS1+="${Ctime} \@ ${Cnorm}|"
+
+	# Exit code, and prompt
+	PS1+="${Cnorm}├──\n╰╴${EXIT}╶╴\$ "
 }
+
+PROMPT_COMMAND=__prompt_command
+
+if [ ! -f ~/.bash/.prompt-langs ]; then
+	prompt-langs
+fi
