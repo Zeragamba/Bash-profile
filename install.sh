0061a19673692db4fc471ad29aff864f9247eec6
Zeragamba <zera@zeragamba.ca>
2022-06-16 09:53:39 -0400

    clean out unneeded files

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sed -i -e '$a\' ~/.bashrc
-echo "source ~/.bash/profile.sh" >> ~/.bashrc
-
-git config --global user.name "Stephen A. Wilson"
-git config --global user.email "Stephen-356@hotmail.com"
-git config --global push.default simple
