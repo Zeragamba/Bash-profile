f59d13025e451b49d002d508f173b7bb54130a48
Zeragamba <Zera@zeragamba.ca>
2022-10-11 10:08:26 -0400

    Remove pyenv setup from shared zsh.sh

--- zsh.sh
+++ zsh.sh
@@ -89,10 +89,6 @@ plugins=(
 
 NVM_AUTOLOAD=1
 
-export PYENV_ROOT="$HOME/.pyenv"
-export PATH="$PYENV_ROOT/bin:$PATH"
-eval "$(pyenv init --path)"
-
 # User configuration
 
 # export MANPATH="/usr/local/man:$MANPATH"
